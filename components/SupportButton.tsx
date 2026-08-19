0e47df808fd0c88eb14b28afc4ead5a8579c47ec
Ryan <shilax@gmail.com>
2026-08-18 18:59:27 -0700

    fix: support button opens Gmail compose

--- components/SupportButton.tsx
+++ components/SupportButton.tsx
@@ -3,7 +3,7 @@
 export default function SupportButton() {
   return (
     <button
-      onClick={() => { window.open('mailto:shilax@gmail.com?subject=CapyNook%20Support') }}
+      onClick={() => { window.open('https://mail.google.com/mail/?view=cm&to=shilax@gmail.com&su=CapyNook+Support', '_blank') }}
       className="fixed bottom-5 right-5 bg-amber-500 hover:bg-amber-600 text-white rounded-full w-12 h-12 shadow-lg flex items-center justify-center text-xl z-50 transition-colors"
       title="Help & Support"
     >
